829856e2204e734f297873f0b67f4dce0e8e21c9
Nikolay Digaev <ffs.cmp@gmail.com>
2017-06-22 17:34:37 +0500

    Add more comments to sample config

--- paycom.config.sample.php
+++ paycom.config.sample.php
@@ -1,9 +1,16 @@
 <?php
 // sample configuration with fake values
 return [
+    // Get it in merchant's cabinet in cashbox settings
     'merchant_id' => '69240ea9058e46ea7a1b806a',
+
+    // Login is always "Paycom"
     'login' => 'Paycom',
+
+    // File with cashbox key (key can be found in cashbox settings)
     'keyFile' => 'password.paycom',
+
+    // Your database settings
     'db' => [
         'database' => '<database name>',
         'username' => '<database username>',
